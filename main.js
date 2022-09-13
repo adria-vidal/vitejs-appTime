95891f35e91ce97a2c620f50fe9b1bd918a9e4c4
adria-vidal <73795195+adria-vidal@users.noreply.github.com>
2022-09-13 22:45:42 +0200

    2 files modified, 2 added

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+const $ = (selector) => document.querySelector(selector);
+function changeTimeZone(date, timeZone): Date {
+  const dateToUse = typeof date === 'string' ? new Date(date) : date;
+}
+return new Date(
+  dateToUse.toLocaleString(' es - ES ', {
+    timeZone,
+  })
+);
 
+// const $$ = selector⇒ document.querySelectorAll ( selector )
+const $form = $('form');
+const spainInfo = countries.find(coun);
+$form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const { date } = Object.fromEntries(new window.FormData($form));
+  const mainDate = new Date(date);
+
+const [mainTimezone] = spainInfo.timezones
+const spainDate = changeTimeZone(mainDate,mainTimezone)
+const colombiaDate = changeTimeZone(mainDate,colombiaInfo)
+const argentinaDate = changeTimeZone(mainDate,mainTimezone)
+
+
+  console.log(mainDate);
+});
